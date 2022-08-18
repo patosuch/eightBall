c958b034fc0c427c2def18e84fe8954d0b3e376f
patosuch <107375616+patosuch@users.noreply.github.com>
2022-08-17 20:52:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// User input name here
+// (USER INPUT) input name here
 var username = '';
 
-// user inputs question for the eightBall here
+// (USER INPUT) inputs question for the eightBall here
 var userQuestion = 'Will I marry Tarzan?';
 
-// if user doesnt input a name this appears
+// if user doesn't input a name this appears
 if (username === ''){
 console.log(`Hello!`);
 }
@@ -27,43 +27,43 @@ var randomNumber = Math.floor(Math.random()*8 );
 // know what is what with each far 
 var eightBall = randomNumber;
 
-// if eightball is equal to 0, the console spits out this statement
+// (OUTPUT) if eightball is equal to 0, the console spits out this statement
 if (eightBall === 0){
     console.log('It is certain , my dear!');
 }
 
-// if eightball is equal to 1, the console spits out this statement
+// (OUTPUT) if eightball is equal to 1, the console spits out this statement
 else if (eightBall === 1){
     console.log('It is decidedly so!');
 }
 
-// if eightball is equal to 2, the console spits out this statement
+// (OUTPUT) if eightball is equal to 2, the console spits out this statement
 else if (eightBall === 2){
     console.log('My head hurts! Reply hazy try again.');
 }
 
-// if eightball is equal to 3, the console spits out this statement
+// (OUTPUT) if eightball is equal to 3, the console spits out this statement
 else if (randomNumber === 3){
     console.log('Off on lunch. Cannot predict now.');
 }
     
-// if eightball is equal to 4, the console spits out this statement
+// (OUTPUT) if eightball is equal to 4, the console spits out this statement
 
 else if (randomNumber === 4){
     console.log('Do not count on it. ');
 }
 
-// if eightball is equal to 5, the console spits out this statement
+// (OUTPUT) if eightball is equal to 5, the console spits out this statement
 else if (randomNumber === 5){
     console.log('My sources say no. Bummer.');
 }
 
-// if eightball is equal to 6, the console spits out this statement
+// (OUTPUT) if eightball is equal to 6, the console spits out this statement
 else if (randomNumber === 6){
     console.log('Outlook not so good.');
 }
 
-// if eightball is equal to 7, the console spits out this statement
+// (OUTPUT) if eightball is equal to 7, the console spits out this statement
 else if (randomNumber === 7){
     console.log('Signs point to yes!!!');
 }
